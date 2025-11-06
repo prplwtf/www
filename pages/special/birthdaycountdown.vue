5a2bfd8b11d2dcb5a08407fac3c2e655cdf18f5b
Emma <hello@prpl.wtf>
2025-11-06 18:33:19 +0100

    feat: im special

---
+++ pages/special/birthdaycountdown.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>emma's super awesome birthday countdown!!!!!!!!</p>
+
+  <AgeCounter
+    :birthTimestamp="1225961400000"
+    :width="200"
+    :height="60"
+    :fontSize="30"
+    textColor="#9399b2"
+  />
+</template>
